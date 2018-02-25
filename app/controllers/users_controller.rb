16d827cc950a4ee0320b82b126d09521c9d21bd3
Anna Boeri <annaboeri@gmail.com>
2018-02-24 19:19:05 -0800

    added sign up form to users new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    return params.require(:user).permit(:name, :email, :password)
+    return params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
