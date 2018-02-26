0da17f5ca731a7d05c66d44c01268ac2fa2c8a59
Anna Boeri <annaboeri@gmail.com>
2018-02-26 09:07:50 -0800

    fixed bug in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     if @user and @user.authenticate(params[:password])
-      sessions[:user_id] = @user.id 
+      session[:user_id] = @user.id 
       redirect_to user_path(@user)
     else
       redirect_to new_session_path
