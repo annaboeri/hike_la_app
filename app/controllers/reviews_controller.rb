7c1fc69f17f79f2bb3283f9f64bc52bc8dfbf4bb
Anna Boeri <annaboeri@gmail.com>
2018-04-24 10:23:18 -0700

    show reviews even if user not logged in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -38,12 +38,13 @@ class ReviewsController < ApplicationController
     redirect_to hikes_path
   end
 
-  def authorize_review_view
-    @review = Review.find params[:id]
-    if @review.user_id != current_user.id
-      redirect_to hikes_path
-    end
-  end
+  # only display reviews if user is logged in:
+  # def authorize_review_view
+  #   @review = Review.find params[:id]
+  #   if @review.user_id != current_user.id
+  #     redirect_to hikes_path
+  #   end
+  # end
 
   private
   def review_params
